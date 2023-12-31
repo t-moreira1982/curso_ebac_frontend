21e15f8f2f4dce98b8233a800f0a86f8bb65f87a
Thiago <thiago_moreira22@hotmail.com>
2023-12-31 12:56:31 -0300

    exercicio ajax

---
+++ exercicio_ajax/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.getElementById('name');
+    const usernameElement = document.getElementById('username');
+    const avatarElement = document.getElementById('avatar');
+    const reposElement = document.getElementById('repos');
+    const followersElement = document.getElementById('followers');
+    const followingElement = document.getElementById('following');
+    const linkElement = document.getElementById('link');
+
+    fetch('https://api.github.com/users/t-moreira1982')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json){
+        nameElement.innerText = json.name;
+        usernameElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        repos.innerHTML = json.public_repos;
+        followersElement.innerText = json.followers;
+        followingElement.innerText = json.following;
+        linkElement.href = json.html_url;
+    })
+
+
+});
